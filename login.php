54eaf6831293bf79e27354484169023bfb423241
jake-whipp <73247616+jake-whipp@users.noreply.github.com>
2022-03-16 22:24:55 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,21 @@
-<?php echo "Hello world!"; ?>
+<?php
+    function SanitizeRequest($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+
+        return $data;
+    }
+
+    if (empty(SanitizeRequest($_POST["Username"]))) {
+        header('Location: ' . "index.php?error=1", true);
+        die();
+    }
+
+    if (empty(SanitizeRequest($_POST["Password"]))) {
+        header('Location: ' . "index.php?error=2", true);
+        die();
+    }
+
+    echo "<h1> Welcome, " . SanitizeRequest($_POST["Username"]) . "!</h1>";
+?>
