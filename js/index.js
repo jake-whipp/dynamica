6b46265d2af4223ebaf5647979f739a6dc848a7f
jake-whipp <73247616+jake-whipp@users.noreply.github.com>
2022-03-19 22:42:34 +0000

    Implemented js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/* index.js
+-----------------------
+
+Main Handler for the login page on dynamica.
+Utilises functions from the jquery-functions module. 
+
+Dynamica - Jacob Whipp (2022).
+
+*/
+
+$(document).ready(function(){ // ensures document loaded before running js
+    LoginToggle();
+});
