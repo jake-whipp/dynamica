b56e529ea6a19a1143c3297b79887ccbdaa3c4b5
jake-whipp <73247616+jake-whipp@users.noreply.github.com>
2022-03-19 22:44:30 +0000

    Added registration, errors cleanup

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!-- index.php
+-----------------------
+
+Splash page for dynamica.
+Serves as login screen and registration screen for users.
+
+Dynamica - Jacob Whipp (2022).
+
+-->
+
 <?php
     function SanitiseRequest($data) {
         $data = trim($data);
@@ -11,40 +21,55 @@
 <html>
     
     <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <!-- Google fonts stuff... -->
+        <link rel="preconnect" href="https://fonts.googleapis.com"> 
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
         <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Bebas+Neue&family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
 
-        <link rel="stylesheet" href="stylesheet.css" />
+
+        <!-- Link to style sheet -->
+        <link rel="stylesheet" href="styles/stylesheet.css" /> 
+
+
+        <!-- JQuery and JQuery UI CDN hosted libraries -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script> 
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+        <!-- linking JQuery module and scripts -->
+        <script src="js/jquery-functions.js"></script> 
+        <script src="js/index.js"></script>
     </head>
 
     <body>
         <center>
             <div>
-                <h1 class="LogoHeader">dynamica</h1>
-                <h4 style="position: relative; font-family: sans-serif; left:100px; color:rgb(43, 144, 184); width:200px;">bringing people together</h4>
+                <h1 class="Logo" style="top:65px;">dynamica</h1>
+                <h4 class="Motto">bringing people together</h4>
             </div>
         </center>
        
 
         <center>
-            <form method="post" action="login.php">
+            <form id="LoginForm" method="post" action="account/login.php">
                 <div class="LoginContainer">
-                    <h3 style="font-family: 'Roboto'; font-weight:400;">Login</h3>
+                    <h3 id="LoginHeader" style="font-family: 'Roboto'; font-weight:400;">Login</h3>
                     <hr style="width:85%; height:1px; border: 0;" class="gradient" />
                     
                     <?php 
                         if (isset($_GET["error"])) {
                             switch(SanitiseRequest($_GET["error"])) {
+                                case "0":
+                                    echo "<p class='ErrorMessage'>Unexpected error</p>";
+                                    break;
                                 case "1":
-                                    echo "<p style='color:red; padding:0; margin:0; position:relative; max-height:0px; font-weight:600;'>No username given!</p>";
+                                    echo "<p class='ErrorMessage'>No username given!</p>";
                                     break;
                                 case "2":
-                                    echo "<p style='color:red; padding:0; margin:0; position:relative; max-height:0px; font-weight:600;'>No password given!</p>";
+                                    echo "<p class='ErrorMessage'>No password given!</p>";
                                     break;
                                 case "3":
-                                    echo "<p style='color:red; padding:0; margin:0; position:relative; max-height:0px; font-weight:600;'>Username or password is incorrect!</p>";
+                                    echo "<p class='ErrorMessage'>Username or password is incorrect!</p>";
                                 default:
                                     break;
                             }
@@ -67,7 +92,7 @@
                 </div>
 
                 <div style="position:absolute; bottom:0; width:99%; padding:0; margin:0;">
-                    <p style="font-family:Roboto; font-weight:300;">no account? <a href="register.php">register</a></p> 
+                    <p style="font-family:Roboto; font-weight:300;">no account? <a class="ToggleLogReg" href="#">register</a></p> 
                 </div>
                 
             </form>
