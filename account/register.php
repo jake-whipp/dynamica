42ca5f9fd2905df2bbd205126c3c80e2c8b65913
jake-whipp <73247616+jake-whipp@users.noreply.github.com>
2022-03-25 22:31:26 +0000

    Cookie / auth compatibility

--- account/register.php
+++ account/register.php
@@ -13,6 +13,9 @@ Dynamica - Jacob Whipp (2022).
 <!-- or email method -->
 
 <?php
+    session_start();
+    $timeout = 900; // how long can the user remain active
+
     function SanitiseRequest($data) {
         $data = trim($data);
         $data = stripslashes($data);
@@ -21,56 +24,109 @@ Dynamica - Jacob Whipp (2022).
     return $data;
     }
 
-    if (empty(SanitiseRequest($_POST["Username"]))) {
-        header('Location: ' . "/index.php?error=1", true);
-        die();
-    } elseif (empty(SanitiseRequest($_POST["Password"]))) {
-        header('Location: ' . "/index.php?error=2", true);
-        die();
-    } 
-    
-    $alnumUser = ctype_alnum(SanitiseRequest($_POST["Username"]));
+    function CreateSession($auth_cookie_name, $auth_cookie_value, $auth_cookie_expiry) {
+        global $timeout;
+        $auth_cookie_created = time();
 
-    if (!($alnumUser)) {
-        header('Location: ' . "/index.php?error=4", true);
-        die();
+        setcookie($auth_cookie_name, $auth_cookie_value, $auth_cookie_expiry, "/"); // Accessed with superglobal $_COOKIE["AUTHCOOKIE"];
+
+
+        // Declare session variables to create a server-client pair
+        $_SESSION["AUTHID"] = $auth_cookie_value;
+        $_SESSION["AUTHEXPIRY"] = $auth_cookie_expiry;
+        $_SESSION["AUTHCREATED"] = $auth_cookie_created;
     }
 
+    function DestroySession() {
+        CreateSession("AUTHCOOKIE", "",(time() - 3600));
+
+        session_unset();
+        session_destroy();
+    }
+
+    function ValidateSession() {
+        if (isset($_SESSION["AUTHEXPIRY"])) {
+            if (time() - $_SESSION["AUTHEXPIRY"] > 0) {
+                // extra time has passed since the expiry date of the auth session
+                DestroySession();
+
+                return 0;
+            } else {
+                if ($_COOKIE["AUTHCOOKIE"] == $_SESSION["AUTHID"]) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        } else {
+            return 0;
+        }
+    }
+
+
+
+    // Establish login details for sql. NOT username logon
     $servername = "localhost";
-    $username = "root"; // login details for sql. NOT username logon
+    $username = "root"; 
     $password = "root";
     $dbname = "dynamica";
 
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    if (ValidateSession()) {
+        header('Location: ' . "/dashboard.php");
+        die();
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    } else { // If user is NOT logged in:
 
-    // Check for existing user
-    $sqlSearchExistingAccounts = "SELECT * FROM users WHERE Username='" . SanitiseRequest($_POST["Username"]) . "';";
-    $result = $conn->query($sqlSearchExistingAccounts);
-
-    if ($result->num_rows > 0) {
-        header('Location: ' . "/index.php?error=5", true);
+        if (empty(SanitiseRequest($_POST["Username"]))) {
+            header('Location: ' . "/index.php?error=1", true);
+            die();
+        } elseif (empty(SanitiseRequest($_POST["Password"]))) {
+            header('Location: ' . "/index.php?error=2", true);
+            die();
+        } 
+        
+        // Check for alphanumerical characters
+        $alnumUser = ctype_alnum(SanitiseRequest($_POST["Username"]));
+    
+        if (!($alnumUser)) {
+            header('Location: ' . "/index.php?error=4", true);
+            die();
+        }
+    
+    
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+    
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+    
+        // Check for existing user
+        $sqlSearchExistingAccounts = "SELECT * FROM users WHERE Username='" . SanitiseRequest($_POST["Username"]) . "';";
+        $result = $conn->query($sqlSearchExistingAccounts);
+    
+        if ($result->num_rows > 0) {
+            header('Location: ' . "/index.php?error=5", true);
+            die();
+        }
+    
+        // Create a new account
+        $sqlCreateNewAccount = "INSERT INTO users (Username, Password) VALUES ('" . SanitiseRequest($_POST["Username"]) . "', '" . SanitiseRequest($_POST["Password"]) . "');";
+    
+        if ($conn->query($sqlCreateNewAccount) === TRUE) {
+            echo "New account created successfully";
+        } else {
+            header('Location: ' . "/index.php?error=0", true);
+            die();
+        }
+    
+        $conn->close();
+    
+        CreateSession("AUTHCOOKIE", SanitiseRequest($_POST["Username"]), time() + $timeout);
+    
+        header("Location: " . "/dashboard.php");
         die();
     }
-
-    // Create a new account
-    $sqlCreateNewAccount = "INSERT INTO users (Username, Password) VALUES ('" . SanitiseRequest($_POST["Username"]) . "', '" . SanitiseRequest($_POST["Password"]) . "');";
-
-    if ($conn->query($sqlCreateNewAccount) === TRUE) {
-        echo "New account created successfully";
-    } else {
-        header('Location: ' . "/index.php?error=0", true);
-        die();
-    }
-
-    $conn->close();
-
-    header("Location: " . "/dashboard.php");
-    die();
 ?>
