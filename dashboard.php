30be38508bb5360b59f39a69dde0c61788e5df17
jake-whipp <73247616+jake-whipp@users.noreply.github.com>
2022-03-25 22:32:04 +0000

    Cookie / auth compatibility

--- dashboard.php
+++ dashboard.php
@@ -9,6 +9,9 @@ Dynamica - Jacob Whipp (2022).
 -->
 
 <?php
+    session_start();
+    $timeout = 900; // how long can the user remain active
+
     function SanitiseRequest($data) {
         $data = trim($data);
         $data = stripslashes($data);
@@ -17,5 +20,81 @@ Dynamica - Jacob Whipp (2022).
     return $data;
     }
 
-    // TODO: If  there's no current auth cookie active for signed in user, then redirect them to /dynamica-main/index.php/
+    function CreateSession($auth_cookie_name, $auth_cookie_value, $auth_cookie_expiry) {
+        global $timeout;
+        $auth_cookie_created = time();
+
+        setcookie($auth_cookie_name, $auth_cookie_value, $auth_cookie_expiry, "/"); // Accessed with superglobal $_COOKIE["AUTHCOOKIE"];
+
+
+        // Declare session variables to create a server-client pair
+        $_SESSION["AUTHID"] = $auth_cookie_value;
+        $_SESSION["AUTHEXPIRY"] = $auth_cookie_expiry;
+        $_SESSION["AUTHCREATED"] = $auth_cookie_created;
+    }
+
+    function DestroySession() {
+        CreateSession("AUTHCOOKIE", "",(time() - 3600));
+
+        session_unset();
+        session_destroy();
+    }
+
+    function ValidateSession() {
+        if (isset($_SESSION["AUTHEXPIRY"])) {
+            if (time() - $_SESSION["AUTHEXPIRY"] > 0) {
+                // extra time has passed since the expiry date of the auth session
+                DestroySession();
+
+                return 0;
+            } else {
+                if ($_COOKIE["AUTHCOOKIE"] == $_SESSION["AUTHID"]) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    function UpdateSession() {
+        global $timeout;
+
+        if (!isset($_SESSION["AUTHCREATED"])) {
+            CreateSession("AUTHCOOKIE", (SanitiseRequest($_POST["Username"]) . "_" . random_int(1000000, 2000000)), (time() + $timeout));
+        } else {
+            session_regenerate_id(true);
+            $_SESSION["AUTHCREATED"] = time();
+            $_SESSION["AUTHEXPIRY"] = time() + $timeout;
+        }
+    }
+
+
+
+    if (ValidateSession()) {
+        UpdateSession();
+    } else {
+        DestroySession();
+        header('Location: ' . "/index.php");
+        die();
+    }
+    
 ?>
+
+<html>
+    <body>
+        <center>
+            <div>
+                <?php
+                    echo "<h1>Hello, " . $_SESSION["AUTHID"] . "!</h1>";
+                ?>
+            </div>
+
+            <form action="account/logout.php">
+                <input type="submit" value="Logout"/>
+            </form>
+        </center>
+    </body>
+</html>
